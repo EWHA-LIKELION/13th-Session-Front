d01fa38327c4e8ea156a2cc61a01059ed2719f0a
3olly <cy.kim@ewhain.net>
2025-05-08 07:51:26 +0900

    fix: React_1 세션 자료 수정

--- React_1/test/src/App.js
+++ React_1/test/src/App.js
@@ -1,8 +1,33 @@
+<<<<<<< HEAD
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return <></>;
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+>>>>>>> c0d3b3d4 (fix: React_1 세션 자료 수정)
 }
 
 export default App;
