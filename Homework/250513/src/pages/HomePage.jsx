51582baf1430b0cc0f83d5984dd4bab5f6588d10
lloviazuu <sungjoo3321@gmail.com>
2025-05-20 01:03:22 +0900

    Homepage

--- Homework/250513/src/pages/HomePage.jsx
+++ Homework/250513/src/pages/HomePage.jsx
@@ -4,19 +4,25 @@ import styled from "styled-components";
 import TopBar from "../components/Topbar";
 
 // props로 받아올 posts 구조 분해 할당
-const HomePage = () => {
-	return (
-		<Wrapper>
-			<TopBar />
-			{/* map을 이용해 배열의 요소를 하나씩 렌더링 */}
-		</Wrapper>
-	);
+const HomePage = (props) => {
+  const { posts } = props;
+  return (
+    <Wrapper>
+      <TopBar />
+      {posts.map((post) => (
+        <PostCard key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+        </PostCard>
+      ))}
+    </Wrapper>
+  );
 };
 
 export default HomePage;
 
 const Wrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
