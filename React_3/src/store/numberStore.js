84617a1e4466275add66873d4d2cb8f71ed831b1
[Min-hye-Kim] <[kimilu0108@gmail.com]>
2025-05-22 19:05:26 +0900

    250520

--- React_3/src/store/numberStore.js
+++ React_3/src/store/numberStore.js
@@ -1,10 +1,10 @@
-import { creat } from "zustand";
+import { create } from "zustand";
 
 const useNumberStore = create((set) => ({
     number:13,
 
     increase: () => set((state) => ({number: state.number + 1 })),
-    decrease: () => set((state) => ({number: state.number + 1 })),
+    decrease: () => set((state) => ({number: state.number - 1 })),
     reset: () => set({number: 13}),
 }));
 
