d733f8f3c5ebff61984affd6b50670482ab2b3fd
lloviazuu <sungjoo3321@gmail.com>
2025-09-17 14:36:32 +0900

    login.jsx수정

--- Homework/Kakao-Login/src/pages/Login.jsx
+++ Homework/Kakao-Login/src/pages/Login.jsx
@@ -1,26 +1,24 @@
 //import { supabase } from "../superbase/client";
 
 export default function Login() {
-	const login = async () => {
-		// TODO: 아래 주석을 보고 코드를 완성하세요.
-		// 1) origin을 구합니다. (힌트: window.location.origin)
-		// 2) supabase.auth.signInWithOAuth를 호출합니다.
-		//    - provider: 'kakao'
-		//    - options.redirectTo: `${origin}/auth/callback?next=/profile`
-		//    - options.scopes: 'account_email profile_nickname profile_image'
+  const login = async () => {
+    // TODO: 아래 주석을 보고 코드를 완성하세요.
+    // 1) origin을 구합니다. (힌트: window.location.origin)
+    // 2) supabase.auth.signInWithOAuth를 호출합니다.
+    //    - provider: 'kakao'
+    //    - options.redirectTo: `${origin}/auth/callback?next=/profile`
+    //    - options.scopes: 'account_email profile_nickname profile_image'
 
-		// 예시)
-		// const origin = ...;
-		// await supabase.auth.signInWithOAuth({
-		// 	provider: 'kakao',
-		// 	options: {
-		// 		redirectTo: `${origin}/auth/callback?next=/profile`,
-		// 		scopes: 'account_email profile_nickname profile_image',
-		// 	},
-		// });
+    const origin = window.location.origin;
+    await supabase.auth.signInWithOAuth({
+      provider: "kakao",
 
-		alert("TODO: 로그인 로직을 구현해 보세요 (파일: Login.jsx)");
-	};
+      redirectTo: `${origin}/auth/callback?next=/profile`,
+      scopes: "account_email profile_nickname profile_image",
+    });
 
-	return <button onClick={login}>카카오로 로그인</button>;
+    //alert("TODO: 로그인 로직을 구현해 보세요 (파일: Login.jsx)");
+  };
+
+  return <button onClick={login}>카카오로 로그인</button>;
 }
